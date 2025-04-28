b4b311fe012e793a933b7c6cb2c9f6d5cb9b68d1
Jason <jasonpalomino@csu.fullerton.edu>
2025-04-28 11:58:13 -0700

    Initial files

---
+++ Professor_Interface.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<?php>
+
+/*
+$link = mysqli_connect('mariadb', 'cs332t29', 'y5pD4Xwt', 'cs332t29');
+if (!$link) {
+        die('Could not connect: '.mysql_error());
+}
+echo 'Connected successfully<p>';
+
+$result = $link->query("SELECT * FROM STUDENT");
+$nor=$result->num_rows;
+
+for($i=0; $i<$nor, $i++)
+{
+        $row=$result->fetch_assoc();
+        echo "SSN: ", $row["ssn"], "<br>";
+        echo "First NAME: ", $row["fname"], "<br>";
+        echo "Last NAME: ", $row["lname"], "<br>";
+}
+
+$result->free_result();
+$link->close();
+?>
+
+*/
+
+</body>
+</html>
